0e47fc31411cee0a1dfed8e4ad7c6416294117d0
Bernardo Damele <bernardo.damele@gmail.com>
2012-12-18 11:44:31 +0000

    more enhancements

--- mysql/get_int_nooutput.php
+++ mysql/get_int_nooutput.php
@@ -2,5 +2,5 @@
     require_once('../libs/mysql.inc.php');
 
     $query = "SELECT * FROM users WHERE id=" . $_GET['id'] . " LIMIT 0, 1";
-    dbQuery($query, $all_results=false);
+    dbQuery($query, false, true, false);
 ?>
