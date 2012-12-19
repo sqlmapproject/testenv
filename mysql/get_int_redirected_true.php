ae0689e9ba973731dd489015146617e4a40d3c1c
Bernardo Damele <bernardo.damele@gmail.com>
2012-12-19 23:33:08 +0000

    fixed another page

--- mysql/get_int_redirected_true.php
+++ mysql/get_int_redirected_true.php
@@ -1,13 +1,15 @@
 <?php
-    function dbQuery($query) {
+    // Show all PHP error messages
+    error_reporting(E_ALL);
+
+    function dbQuery($query, $show_errors=true, $all_results=true, $show_output=true) {
         // Connect to the MySQL database management system
-        // NOTE: it is installed on localhost
-        $link = mysql_pconnect("localhost", "root", "");
+        $link = mysql_pconnect("localhost", "root", "testpass");
         if (!$link) {
             die(mysql_error());
         }
 
-        // Make 'test' the current database
+        // Make 'testdb' the current database
         $db_selected = mysql_select_db("testdb");
         if (!$db_selected) {
             die (mysql_error());
@@ -17,15 +19,14 @@
         $result = mysql_query($query);
 
         if (!$result or !mysql_num_rows($result)) {
-            print "<b>SQL error:</b> ". mysql_error() . "<br>\n";
+            if ($show_errors)
+                print "<b>SQL error:</b> ". mysql_error() . "<br>\n";
             exit(1);
         }
-        else
-        {
-            header("Location: .");
-        }
+
+        header("Location: /");
+        exit(1);
     }
 
     $query = "SELECT * FROM users WHERE id=" . $_GET['id'] . " LIMIT 0, 1";
-    dbQuery($query);
-?>
+    dbQuery($query);?>
