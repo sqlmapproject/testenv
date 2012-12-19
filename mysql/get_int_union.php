1dcd3b023fdba732431d15cd6fd5bd9ba005283e
Bernardo Damele <bernardo.damele@gmail.com>
2012-12-19 23:55:10 +0000

    removed

--- mysql/get_int_union.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('../libs/mysql.noerror.inc.php');
-
-    if (strripos($_GET['id'], 'AND ')) {
-    die ("DAMN YOU HACKERS");
-    }
-    $query = "SELECT * FROM users WHERE id=" . $_GET['id'] . " LIMIT 0, 1";
-    dbQuery($query);
-?>
