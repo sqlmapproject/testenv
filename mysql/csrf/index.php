fc7a83dd802f13e01323badb5bf1dcec822193f6
Bernardo Damele <bernardo.damele@gmail.com>
2012-12-20 10:32:30 +0000

    code restyling

--- mysql/csrf/index.php
+++ mysql/csrf/index.php
@@ -1,20 +1,21 @@
-<?php	
-session_start();
+<?php
+    session_start();
 
-function generate_secret_token() {
-	 return uniqid(time());
-}
+    function generate_secret_token() {
+	     return uniqid(time());
+    }
 
-$sSecretToken = generate_secret_token();
+    $sSecretToken = generate_secret_token();
 
-$_SESSION['anticsrf_token'] = $sSecretToken;
-
+    $_SESSION['anticsrf_token'] = $sSecretToken;
 ?>
 
-
 <html>
+<body>
 <form method="POST" name="csrfguard_sample" action="post.php">
   <input type="text" name="id">
   <input type="hidden" name="secret_token" value="<?php echo $sSecretToken; ?>">
   <input type="submit" value="Submit">
 </form>
+</body>
+</html>
--- mysql/csrf/post.php
+++ mysql/csrf/post.php
@@ -1,33 +1,33 @@
-<?php	
-session_start();
+<?php
+    session_start();
 
-function fool() {
-   print uniqid(time());
-   return;	 
-}
-
-
-function extract_token_from_session() { 
-   $token = $_SESSION['anticsrf_token'];
-   unset ($_SESSION['anticsrf_token']);
-   return $token;
-}
+    function fool() {
+       print uniqid(time());
+       return;	 
+    }
 
 
-if (! isset($_SESSION['anticsrf_token'])) {
-   fool();
-   return;
-}
+    function extract_token_from_session() { 
+       $token = $_SESSION['anticsrf_token'];
+       unset ($_SESSION['anticsrf_token']);
+       return $token;
+    }
 
-$secret_token = extract_token_from_session();
 
-if ($secret_token != $_POST['secret_token']) {
-   fool();
-   return;
-}
+    if (! isset($_SESSION['anticsrf_token'])) {
+       fool();
+       return;
+    }
 
-require_once('../../libs/mysql.inc.php');
+    $secret_token = extract_token_from_session();
 
-$query = "SELECT * FROM users WHERE id=" . $_POST['id'] . " LIMIT 0, 1";
-dbQuery($query);
+    if ($secret_token != $_POST['secret_token']) {
+       fool();
+       return;
+    }
+
+    require_once('../../libs/mysql.inc.php');
+
+    $query = "SELECT * FROM users WHERE id=" . $_POST['id'] . " LIMIT 0, 1";
+    dbQuery($query);
 ?>
