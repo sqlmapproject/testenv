63e34452400fe6d0e8418b1ce1c333563d0b518c
Bernardo Damele <bernardo.damele@gmail.com>
2014-01-10 17:01:27 +0000

    fixed Informix lib to use PDO

--- libs/informix.inc.php
+++ libs/informix.inc.php
@@ -1,5 +1,12 @@
 <?php
     function dbQuery($query, $show_errors=true, $all_results=true, $show_output=true) {
+        putenv("INFORMIXDIR=/opt/IBM/informix");
+        putenv("INFORMIXSERVER=ol_informix1170");
+        putenv("ONCONFIG=onconfig.ol_informix1170");
+        putenv("INFORMIXSQLHOSTS=/opt/IBM/informix/etc/sqlhosts.ol_informix1170");
+        putenv("LD_LIBRARY_PATH=/opt/IBM/informix/lib:/opt/IBM/informix/lib/cli:/opt/IBM/informix/lib/esql:/opt/IBM/informix/lib/tools");
+        putenv("ODBCINI=/etc/odbc.ini");
+
         if ($show_errors)
             error_reporting(E_ALL);
         else
@@ -7,9 +14,13 @@
 
         // Connect to the Informix database management system
         // NOTE: it is installed on localhost
-        $link = ifx_pconnect("online_root@localhost", "", "");
-        if (!$link) {
-            die(ifx_error());
+        try {
+            // Call the PDO class.
+            $link= new PDO('informix:DSN=inf');
+        }
+        catch(PDOException $e) {
+            // If something goes wrong, PDO throws an exception with a nice error message.
+            echo $e->getMessage();
         }
 
         // Print results in HTML
@@ -19,12 +30,11 @@
         //print "<b>SQL query:</b> " . $query . "<br>\n";
 
         // Perform SQL injection affected query
-        $result = ifx_query($query, $link);
+        $stmt = $link->query($query);
+        $stmt->setFetchMode(PDO::FETCH_COLUMN);
 
-        if (!$result) {
-            if ($show_errors)
-                print "<b>SQL error:</b> ". ifx_error() . "<br>\n";
-            exit(1);
+        if (!$stmt) {
+             exit(1);
         }
 
         if (!$show_output)
@@ -33,12 +43,12 @@
         print "<b>SQL results:</b>\n";
         print "<table border=\"1\">\n";
 
-        while ($line = ifx_fetch_row($result)) {
+        while($row = $stmt->fetch()) {
             print "<tr>";
-            foreach ($line as $col_value) {
-                print "<td>" . $col_value . "</td>";
-            }
-            print "</tr>\n";
+            echo "<td>" . $row[0] . "</td>";
+            echo "<td>" . $row[1] . "</td>";
+            echo "<td>" . $row[2] . "</td>";
+            echo "</tr>\n";
             if (!$all_results)
                 break;
         }
