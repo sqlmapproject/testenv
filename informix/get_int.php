7b4f8b0905d816fec39847702bfee228431a260a
Bernardo Damele <bernardo.damele@gmail.com>
2015-03-01 20:29:47 +0000

    columns apparently need to be specified

--- informix/get_int.php
+++ informix/get_int.php
@@ -1,6 +1,6 @@
 <?php
     require_once('../libs/informix.inc.php');
 
-    $query = "SELECT * FROM users WHERE id=" . $_GET['id'];
+    $query = "SELECT id, name, surname FROM users WHERE id=" . $_GET['id'];
     dbQuery($query);
 ?>
