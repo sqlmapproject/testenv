ff3f0cd730a5d93cd16187ba3e517ea2528104dc
Andres Riancho <andres.riancho@gmail.com>
2015-07-23 18:34:46 -0300

    Wait for mysql to start

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
-set -e
+# Wait for database to get available
+MYSQL_LOOPS="10"
+MYSQL_HOST="mysql"
+MYSQL_PORT="3306"
+
+# Wait for mysql
+i=0
+while ! nc ${MYSQL_HOST} ${MYSQL_PORT} >/dev/null 2>&1 < /dev/null; do
+  i=`expr $i + 1`
+  if [ ${i} -ge ${MYSQL_LOOPS} ]; then
+    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
+    exit 1
+  fi
+  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}..."
+  sleep 1
+done
+
 
 echo "Create the database"
-mysql -u root -ptestpass mysql < /var/www/sqlmap/schema/mysql.sql
+mysql -u root -ptestpass -h mysql mysql < /var/www/sqlmap/schema/mysql.sql
+
 
 echo "Start apache"
 # Apache gets grumpy about PID files pre-existing
